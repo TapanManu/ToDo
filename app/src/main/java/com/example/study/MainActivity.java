a27319838fe12b087664fbafc81e6feb62c3fe32
Tapan <tapanmanu2000@gmail.com>
2020-08-07 21:11:59 +0530

    Notifications completed

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -19,8 +19,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String CHANNEL_ID = "CHANNEL_ID";
-    private static final String TAG = "main";
+    Notifications n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,66 +34,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         TextView tv = findViewById(R.id.tv);
         ConstraintLayout layout = findViewById(R.id.cons);
-        Notifications n = new Notifications();
         switch(item.getItemId()){
             case R.id.first:
                 tv.setText("first");
                 layout.setBackgroundColor(Color.GREEN);
-                addNotification("BACKGROUND CHANGED","New Color is GREEN",0);
+                new Notifications(this,"BACKGROUND CHANGED","New Color is GREEN",0);
                 break;
             case R.id.second:
                 tv.setText("second");
                 layout.setBackgroundColor(Color.BLUE);
-                addNotification("BACKGROUND CHANGED","New Color is BLUE",1);
+                new Notifications(this,"BACKGROUND CHANGED","New Color is BLUE",1);
                 break;
             case R.id.third:
                 tv.setText("third");
                 layout.setBackgroundColor(Color.RED);
-                addNotification("BACKGROUND CHANGED","New Color is RED",2);
+                new Notifications(this,"BACKGROUND CHANGED","New Color is RED",2);
                 break;
             default:
                 tv.setText("Just for study!!");
                 layout.setBackgroundColor(Color.WHITE);
         }
         return true;
-    }
-    public void addNotification(String title,String text,int notificationid){
-        createNotificationChannel();
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        Intent notification = new Intent(this, MainActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(MainActivity.class);
-
-        stackBuilder.addNextIntent(notification);
-
-        //TaskStackBuilder stackBuilder;
-        PendingIntent resultpendingintent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(resultpendingintent);
-
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        /* notificationID allows you to update the notification later on. */
-        mNotificationManager.notify(notificationid,builder.build());
-
-    }
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 
 }
--- app/src/main/java/com/example/study/Notifications.java
+++ app/src/main/java/com/example/study/Notifications.java
@@ -1,24 +1,35 @@
 package com.example.study;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
-import com.example.study.MainActivity;
-
 public class Notifications extends Activity {
-    public void addNotification(String title,String text){
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(Notifications.this)
-                                                    .setContentTitle(title)
-                                                    .setContentText(text);
+    private static final String CHANNEL_ID = "CHANNEL_ID";
+    private NotificationManager mNotificationManager;
+    public Notifications(Context context,String title,String text,int notificationid){
+        mNotificationManager = (NotificationManager) context.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        addNotification(context,title,text,notificationid);
+    }
 
-        Intent notification = new Intent(Notifications.this, MainActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(Notifications.this);
+    public void addNotification(Context context,String title,String text,int notificationid){
+        createNotificationChannel(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Intent notification = new Intent(context, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
         stackBuilder.addParentStack(MainActivity.class);
 
         stackBuilder.addNextIntent(notification);
@@ -27,9 +38,25 @@ public class Notifications extends Activity {
         PendingIntent resultpendingintent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(resultpendingintent);
 
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         /* notificationID allows you to update the notification later on. */
-        mNotificationManager.notify(0,builder.build());
+        mNotificationManager.notify(notificationid,builder.build());
 
+    }
+    private void createNotificationChannel(Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = context.getApplicationContext().getString(R.string.channel_name);
+            String description = context.getApplicationContext().getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            NotificationManager notificationManager = context.getApplicationContext().
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
